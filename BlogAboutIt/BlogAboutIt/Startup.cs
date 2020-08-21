0443fad82b4f45374cc8fdb35b40ecec71b7f21c
Tiffany Adams <tadams03@mail.roosevelt.edu>
2020-08-20 22:59:00 -0500

    Add CRUD View(s) logic

--- BlogAboutIt/BlogAboutIt/Startup.cs
+++ BlogAboutIt/BlogAboutIt/Startup.cs
@@ -54,7 +54,7 @@ namespace BlogAboutIt
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Posts}/{action=Index}/{id?}");
             });
         }
     }
