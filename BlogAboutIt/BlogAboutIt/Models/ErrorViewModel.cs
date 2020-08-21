77fadcfb75223db4d3c8c226763352f9ddf8e0a4
Tiffany Adams <tadams03@mail.roosevelt.edu>
2020-08-20 19:08:16 -0500

    Create new project

---
+++ BlogAboutIt/BlogAboutIt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogAboutIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
